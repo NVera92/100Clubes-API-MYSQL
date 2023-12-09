a89861a7d79b87c37bd439e2900311d45a135c66
Nicolas Vera <nive7792@gmail.com>
2023-12-09 16:09:49 -0300

    feat: express and BBDD working good

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mySql = require('mysql')
+const myConn = require('express-myconnection')
+const routes = require('./routes')
+
+const app = express()
+app.set('port',process.env.PORT ||9000)
+const dbOptions = {
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: '100clubes'
+}
+
+
+// middlewares ------------------------
+app.use(myConn(mySql,dbOptions,'single'))
+
+// routes -----------------------------
+app.get('/',(req,res) =>{
+    res.send('Welcome to my API');
+})
+
+app.use('/api',routes)
+
+
+// server running ---------------------
+app.listen(app.get('port'), ()=>{
+    console.log('server running on port', app.get('port'))
+})
