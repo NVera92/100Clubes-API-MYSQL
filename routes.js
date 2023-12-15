317560b11617c34a60664ec916e8275fe1aa5b41
Nicolas Vera <nive7792@gmail.com>
2023-12-14 22:08:43 -0300

    feat: Get club by name

--- routes.js
+++ routes.js
@@ -14,5 +14,27 @@ routes.get('/',(req,res)=>{
     })
 })
 
+routes.get('/id',(req,res)=>{
+    req.getConnection((err,conn)=>{
+        if(err) return res.send(err)
 
+        conn.query('SELECT * FROM club WHERE idClub ='+req.query.id,(err,row)=>{
+        
+            if(err) return res.send(err)
+            res.json(row)
+        })
+    })
+})
+
+
+routes.get('/name',(req,res)=>{
+    req.getConnection((err,conn)=>{
+        if(err) return res.send(err)
+
+        conn.query("SELECT * FROM club WHERE nombre LIKE '%"+req.query.name+"%'",(err,row)=>{
+            if(err) return res.send(err)
+            res.json(row)
+        })
+    })
+})
 module.exports = routes
