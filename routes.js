88c4b394137a5b89fbc2142c881106baa3002668
Nicolas Vera <nive7792@gmail.com>
2023-12-17 18:03:34 -0300

    feat: Get clubs by activities

--- routes.js
+++ routes.js
@@ -48,4 +48,15 @@ routes.get('/barrio',(req,res)=>{
         })
     })
 })
+
+routes.get('/actividades',(req,res)=>{
+    req.getConnection((err,conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('CALL obtenerClubesActividades(?)',req.query.actividades,(err,rows)=>{
+            if(err) return res.send(err)
+            res.json(rows)
+        })
+    })
+})
 module.exports = routes
